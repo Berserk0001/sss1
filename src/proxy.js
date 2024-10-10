7f6755a10897a51ca2aef51f3488c9cde7fde551
Rushs321 <fyggh566@tutamail.com>
2024-10-10 14:30:45 +0530

    Update proxy.js

--- src/proxy.js
+++ src/proxy.js
@@ -7,6 +7,17 @@ const compress = require("./compress");
 const copyHeaders = require("./copyHeaders");
 
 async function proxy(request, reply) {
+  let url = request.query.url;
+  if (!url) {
+    reply.send('bandwidth-hero-proxy');
+    return;
+  }
+
+  request.params.url = decodeURIComponent(url);
+  request.params.webp = !request.query.jpeg;
+  request.params.grayscale = request.query.bw != 0;
+  request.params.quality = parseInt(request.query.l, 10) || DEFAULT_QUALITY;
+}
   // Redirect if the request is from the Bandwidth-Hero extension itself
   if (request.headers["via"] === "1.1 bandwidth-hero" && ["127.0.0.1", "::1"].includes(request.headers["x-forwarded-for"] || request.ip)) {
     return redirect(request, reply);
