e587576916fbbcbea589f8799b49da9401d7949a
Rushs321 <fyggh566@tutamail.com>
2024-10-10 14:36:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,30 +1,12 @@
 #!/usr/bin/env node
 'use strict';
 
-const fastify = require('fastify')({ logger: true });
+const fastify= require('fastify')({ logger: true , trustProxy: true});
 const params = require('./src/params');
 const proxy = require('./src/proxy');
 
 
-
-// Route for proxying
-fastify.get('/', async (request, reply) => {
-  await params(request, reply);
-  await proxy(request, reply);
-  reply.send();
-});
-
-// Favicon route (Fastify automatically handles favicon requests, but here's how you can handle it)
-fastify.get('/favicon.ico', async (request, reply) => {
-  reply.code(204).send();
-});
-
-// Start server
 const PORT = process.env.PORT || 8080;
-fastify.listen({ port: PORT }, (err, address) => {
-  if (err) {
-    fastify.log.error(err);
-    process.exit(1);
-  }
-  fastify.log.info(`Worker ${process.pid}: Listening on ${address}`);
-});
+
+fastify.get('/', proxy);
+fastify.listen({host: '0.0.0.0', port: PORT });
